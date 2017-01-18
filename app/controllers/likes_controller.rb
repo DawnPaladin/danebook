85ab9d97e3b9e68e6ad920afa890e78b9af527f3
James Harris <computersherpa@gmail.com>
2017-01-18 17:57:25 -0600

    Async like/unlike for posts and comments

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,22 +6,36 @@ class LikesController < ApplicationController
   def create
     @likable = extract_likable
     @like = @likable.likes.build(user_id: current_user.id)
-    if @like.save
-      flash[:success] = "You now like that #{@likable.class}!"
-      redirect_back(fallback_location: user_posts_path(current_user))
-    else
-      flash[:warning] = @like.errors.full_messages
-      redirect_back(fallback_location: user_posts_path(current_user))
+    @likable_class = @likable.class.to_s.downcase
+    respond_to do |format|
+      if @like.save
+        format.js
+        format.html do
+          flash[:success] = "You now like that #{@likable.class}!"
+          redirect_back(fallback_location: user_posts_path(current_user))
+        end
+      else
+        flash[:warning] = @like.errors.full_messages
+        redirect_back(fallback_location: user_posts_path(current_user))
+      end
     end
   end
 
   def destroy
-    if my_like.destroy
-      flash[:success] = "You no longer like that #{extract_likable.class}."
-      redirect_back(fallback_location: user_posts_path(current_user))
-    else
-      flash[:warning] = "Could not unlike that #{extract_likable.class}."
-      redirect_back(fallback_location: user_posts_path(current_user))
+    @likable = extract_likable
+    @like = my_like
+    @likable_class = @likable.class.to_s.downcase
+    respond_to do |format|
+      if my_like.destroy
+        format.js
+        format.html do
+          flash[:success] = "You no longer like that #{extract_likable.class}."
+          redirect_back(fallback_location: user_posts_path(current_user))
+        end
+      else
+        flash[:warning] = "Could not unlike that #{extract_likable.class}."
+        redirect_back(fallback_location: user_posts_path(current_user))
+      end
     end
   end
 
