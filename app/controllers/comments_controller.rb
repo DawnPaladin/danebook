623b12b9af577345d58ffc9e33c3b1a8b2bbe3ca
James Harris <computersherpa@gmail.com>
2017-01-18 16:33:17 -0600

    Async create/delete for posts and comments. Labels for gender on front page.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,29 +10,50 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(strong_comment_params)
     @comment.user = current_user
-    if @comment.save
-      flash[:success] = "Comment published!"
-      redirect_back(fallback_location: user_posts_path(@comment.user))
-    else
-      flash[:warning] = "Comment could not be published."
-      redirect_back(fallback_location: user_posts_path(@comment.user))
+    respond_to do |format|
+      if @comment.save
+        format.js
+        format.html do
+          flash[:success] = "Comment published!"
+          redirect_back(fallback_location: user_posts_path(@comment.user))
+        end
+      else
+        format.js do
+          flash.now[:warning] = "Comment could not be published."
+        end
+        format.html do
+          flash[:warning] = "Comment could not be published."
+          redirect_back(fallback_location: user_posts_path(@comment.user))
+        end
+      end
     end
   end
 
   def destroy
     @comment = Comment.find(params[:id])
     comment_user = @comment.user
-    if comment_user == current_user
-      if @comment.destroy
-        flash[:success] = "Comment deleted."
-        redirect_back(fallback_location: user_posts_path(comment_user))
+    respond_to do |format|
+      if comment_user == current_user
+        if @comment.destroy
+          format.js
+          format.html do
+            flash[:success] = "Comment deleted."
+            redirect_back(fallback_location: user_posts_path(comment_user))
+          end
+        else
+          format.js do
+            flash.now[:warning] = @comment.errors.full_messages
+            render 'shared/_flash', locals: {flash: flash}
+          end
+          format.html do
+            flash[:warning] = "Comment could not be deleted."
+            redirect_back(fallback_location: user_posts_path(comment_user))
+          end
+        end
       else
-        flash[:warning] = "Comment could not be deleted."
+        flash[:warning] = "You can only delete your own comments."
         redirect_back(fallback_location: user_posts_path(comment_user))
       end
-    else
-      flash[:warning] = "You can only delete your own comments."
-      redirect_back(fallback_location: user_posts_path(comment_user))
     end
   end
 
